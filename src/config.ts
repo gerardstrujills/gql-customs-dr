921fc38f75f0f6a2fcf5db05a1632a59ca64eae7
gerardstrujills <gtrujillo@cfbd.co>
2026-01-18 20:33:51 -0500

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import { DataSource } from "typeorm";
+import { Entry } from "./schemas/entry";
+import { Product } from "./schemas/product";
+import { Supplier } from "./schemas/supplier";
 import { User } from "./schemas/user";
 import { Withdrawal } from "./schemas/withdrawal";
-import { Supplier } from "./schemas/supplier";
-import { Product } from "./schemas/product";
-import { Entry } from "./schemas/entry";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: "postgresql://postgres.ufaelbayeikqixhjgwqv:gg74544019.@aws-0-us-east-1.pooler.supabase.com:6543/postgres?pgbouncer=true",
+  url: "postgresql://postgres.xoirnpngrbefbwgeditj:vzBKWqeXnm1sI9XB@aws-0-us-west-2.pooler.supabase.com:5432/postgres?pgbouncer=true",
   logging: true,
   synchronize: true,
   entities: [User, Withdrawal, Supplier, Product, Entry],
